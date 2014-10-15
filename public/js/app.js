fa47c0be9ff53f898d79509ba708d5e78f123a2f
Mark Keele <mkeele@indiana.edu>
2014-10-15 15:09:29 -0400

    make individual service requests, need to handle promise

--- public/js/app.js
+++ public/js/app.js
@@ -11,26 +11,52 @@ angular.module('measurementApp', ['ngRoute', 'ngAnimate',
   'PortService', 'SocketService', 'EodnCtrl', 'DepotCtrl', 'DepotService']
   ).run(function($rootScope, $http, $timeout) {
     $http.get('/api/services').success(function(data) {
-      console.log('HTTP Service Request: ' , data);
+      Socket.emit("service_request",{});
 
-      // set timer value
-      onTimeout = function() {
-        for(var i = 0; i < data.length; i++) {
-          if(data[i].ttl <= 0) {
-            data[i].status = 'Unknown';
-          } else {
-            data[i].ttl--;
-          }
+      console.log('HTTP Service Request: ' , data);
+      console.log(data.length);
+
+      var unqiueServices = [];
+      var services = [];
+
+      for(var i = 0; i < data.length; i++) {
+        if(unqiueServices.indexOf(data[i].id) == -1) {
+          unqiueServices.push(data[i].id);
         }
-        //continue timer
-        timeout = $timeout(onTimeout,1000);
       }
 
-      // start timer
-      var timeout = $timeout(onTimeout,1000);
+      console.log(unqiueServices.length);
+      console.log(unqiueServices);
 
-      // apply data to scope
-      $rootScope.services = data;
+      for(var i = 0; i < unqiueServices.length; i++) {
+        console.log(unqiueServices[i]);
+
+        $http.get('/api/services/' + unqiueServices[i]).success(function(data) {
+          console.log('HTTP Service Request: ' , data);
+          services.push(data);
+          console.log(services.length);
+          console.log(services);
+
+          // set timer value
+          /*onTimeout = function() {
+            for(var i = 0; i < services.length; i++) {
+              if(services[i].ttl <= 0) {
+                services[i].status = 'Unknown';
+              } else {
+                services[i].ttl--;
+              }
+            }
+            //continue timer
+            timeout = $timeout(onTimeout,1000);
+          }
+
+          // start timer
+          var timeout = $timeout(onTimeout,1000);*/
+
+          // apply data to scope
+          $rootScope.services = services;
+        });
+      }
     });
 
     $http.get('/api/nodes').success(function(data) {
