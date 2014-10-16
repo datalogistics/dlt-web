674df15f44bf633e6dc6e03f47981fc064473a7d
Mark Keele <mkeele@indiana.edu>
2014-10-15 22:55:23 -0400

    fix data socket request

--- public/js/services/DepotService.js
+++ public/js/services/DepotService.js
@@ -6,65 +6,6 @@
 
 angular.module('DepotService', []).service('Depot', function($http, Socket) {
 
-  Socket.emit("depots_request",{});
-
-  /*this.getServices = function(services) {
-    $http.get('/api/services').success(function(data) {
-      console.log('HTTP Service Request: ' , data);
-
-      // set timer value
-      onTimeout = function() {
-        for(var i = 0; i < stateServices.length; i++) {
-          if(stateServices[i].ttl <= 0) {
-            stateServices[i].status = 'Unknown';
-          } else {
-            stateServices[i].ttl--;
-          }
-        }
-        //continue timer
-        timeout = $timeout(onTimeout,1000);
-      }
-
-      // add data
-      stateServices = stateServices.concat(data);
-
-      // start timer
-      var timeout = $timeout(onTimeout,1000);
-
-      // return data
-      services(data);
-    }).error(function(data) {
-      console.log('HTTP Service Error: ' ,  data);
-    });
-  };*/
-
-  /*this.getNodes = function(nodes) {
-    $http.get('/api/nodes').success(function(data) {
-      console.log('HTTP Node Request: ' , data);
-      nodes(data);
-    }).error(function(data) {
-      console.log('HTTP Node Error: ' , data);
-    });
-  };*/
-
-  /*this.getMeasurements = function(measurements) {
-    $http.get('/api/measurements').success(function(data) {
-      console.log('Measurement Request: ' + data);
-      measurements(data);
-    }).error(function(data) {
-      console.log('Measurement Error: ' + data);
-    });
-  };*/
-
-  /*this.getMetadatas = function(metadata) {
-    $http.get('/api/metadata').success(function(data) {
-      console.log('Metadata Request: ' + data);
-      metadata(data);
-    }).error(function(data) {
-      console.log('Metadata Error: ' + data);
-    });
-  };*/
-
   this.getMetadata = function(id, metadata) {
     $http.get('/api/metadata/' + id)
       .success(function(data) {
@@ -77,12 +18,14 @@ angular.module('DepotService', []).service('Depot', function($http, Socket) {
   };
 
   this.getData = function(id, data) {
+    Socket.emit('data_request', {id: id});
+
     $http.get('/api/data/' + id).success(function(data_request) {
       console.log('Data Request: ' + data_request);
       data(data_request);
 
-      Socket.on('depots_data',function(data){
-        console.log('depots Data Request: ' , data);
+      Socket.on('data_data',function(data) {
+        console.log('Depot Data Request: ' , data);
         data(data_request);
       });
     }).error(function(data_request) {
