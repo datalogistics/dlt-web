651e7728de334e69fd8fbdd4cc559d6c0e4cbebf
Ezra Kissel <ezkissel@indiana.edu>
2015-01-21 11:23:57 -0500

    Updated download viz test.

--- app/socketTester.js
+++ app/socketTester.js
@@ -5,22 +5,40 @@ var msg = {
 		p : 'eodnDownload_pushData'
 };
 sock.on('connect' , function(){
-	console.log('connected successfully ');
-	sock.emit(msg.r , {
-		hashId : 1244 ,
-		filename : "helooooo.png",
-		totalSize : 100001 ,
-		connections : 10 
+    console.log('connected successfully ');
+    sock.emit(msg.r , {
+	hashId : 1244,
+	filename : "test_image.tiff",
+	totalSize : 16777216,
+	connections : 4
+    });
+    var off = 0;
+    setInterval(function(){
+	sock.emit(msg.p, { 
+	    hashId : 1244,
+	    ip : 'dresci.incntre.iu.edu',
+	    offset : off,
+	    progress : 65536
 	});
-	
-	setInterval(function(){
-		// Send progress after every 5 seconds 
-		sock.emit(msg.p , { 
-			hashId : 1244 ,
-			ip : '173.194.123.46',
-			offset : 100 ,
-			progress : 100
-		});
-	},5000);	
+	sock.emit(msg.p, {
+	    hashId : 1244,
+	    ip : '152.54.15.19',
+	    offset : off + 65536,
+	    progress : 32768
+	});
+	sock.emit(msg.p, {
+	    hashId : 1244,
+	    ip : 'depot1.loc1.tacc.reddnet.org',
+	    offset : off + 98304,
+	    progress : 49152
+	});
+	sock.emit(msg.p, {
+	    hashId : 1244,
+	    ip : 'depot1.loc1.umich.reddnet.org',
+	    offset : off + 98304,
+	    progress : 262144
+	});
+	off = off + 360448;
+    },1000);
 });
 
