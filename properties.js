aea22036c5a70812575c3cf9abc7fac9f15d2b6b
Prakash <prak5190@yahoo.co.in>
2015-01-27 23:29:53 -0500

    Added support for key files as well

--- properties.js
+++ properties.js
@@ -1,4 +1,10 @@
 /* Configuration file - Configure all source info here .. remove all URL related info from other files */
+var fs = require('fs');
+var unis_cert = './dlt-client.pem';
+var unis_key  = './dlt-client.pem';
+var  key  = fs.readFileSync(unis_key),
+     cert = fs.readFileSync(unis_cert);
+
 var self = {
     ms : {
         // Default is defined at bottom
@@ -30,15 +36,43 @@ var self = {
            port : "9000"
         }
     },
+    keyCertMap : {
+        "dev" : {
+            key : key ,
+            cert : cert
+        }
+    },
+    prodOptions : {
+      key: fs.readFileSync(unis_key),
+      cert: fs.readFileSync(unis_cert),
+      requestCert: true,
+      rejectUnauthorized: false
+    },
     getHttpOptions : function (cfg) {
         var unis = self.unis,
-            ms = self.ms;
+            ms = self.ms,
+            keys = self.keyCertMap;
         // Default is Unis dev
-        var host = unis.dev.url , port = unis.dev.port;
+        // Depending to no. of options se
+        var hostList = ['dev'] ;
+        var hostArr = [] , portArr = [] , keyArr = [] , certArr = [];
+        // Create options according to hosts 
+        hostList.map(function(x){
+            switch (x) {
+                case 'dev' : 
+                hostArr.push(unis.dev.url); portArr.push(unis.dev.port);
+                keyArr.push(keys['dev'].key);certArr.push(keys['dev'].key);
+                break;
+                // Add more 
+            };
+        });
+        
         var httpOptions = {
-            hostname: host,
-            port: port,    
+            hostname: hostArr,
+            port: portArr,
             method: 'GET',
+            keyArr : keyArr ,
+            certArr : certArr,
             headers: {
                 'Content-type': 'application/perfsonar+json',
                 'connection': 'keep-alive'
--- public/js/controllers/FilesCtrl.js
+++ public/js/controllers/FilesCtrl.js
@@ -2,9 +2,38 @@
  * Files Page Controller
  * public/js/controllers/
  */
-
 angular.module('FilesCtrl', []).controller('FilesController', function($scope, $routeParams, $location, $rootScope, Depot, Socket) {
     $scope.fileViewer = 'Please select a file to view its contents';
+    $scope.treeModel = [{
+        "id": "ajson1",
+        "parent": "#",
+        "text": "Simple root dddnode"
+    }, {
+        "id": "ajson2",
+        "parent": "#",
+        "text": "Root node 2"
+    }, {
+        "id": "ajson4",
+        "parent": "ajson2",
+        "text": "Child 2"
+    }];
+    $scope.openNodeCB = function(){
+        $scope.$apply(function(){
+            $scope.treeModel[1].state = {opened : true };
+            $scope.treeModel[0].text = "Hail Angular";
+            $scope.treeModel.push({
+                "id": "ajson3",
+                "parent": "ajson2",
+                "text": "Child 1"
+            });
+            $scope.treeModel.push({
+                "id": "ajson14",
+                "parent": "ajson2",
+                "text": "Chil111111111d 2"
+            });
+        });
+    };
+    
     $scope.nodeSelected = function(e, data) {
         var _l = data.node.li_attr;
         if (_l.isLeaf) {
