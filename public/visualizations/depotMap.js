8e5d88969bdaacc2775da577ccc709e06d098700
Joseph Cottam <jcottam@indiana.edu>
2015-02-04 17:17:37 -0500

    Sidebar showing items with unkown location.

--- public/visualizations/depotMap.js
+++ public/visualizations/depotMap.js
@@ -18,40 +18,60 @@ function allServiceData(baseUrl, serviceIds, then) {
       var url = baseUrl + "/" + service
       q.defer(d3.json, url)
     })
-
+	 
   q.awaitAll(function(error, result) {
-    console.log("Queue done.")
     var services = []
     result.forEach(function(raw) {
+      
+      id = "unknown"
+      if (typeof raw.accessPoint != 'undefined') {
+        id = ((raw.accessPoint || "").split("://")[1] || "").split(":")[0] || "" 
+      }
+
+      place = []
       if (typeof raw.location != 'undefined'
-        && typeof raw.location.institution != 'undefined'
-        && typeof raw.status != 'undefined') {
-            services.push({
-              name: raw.location.institution, 
-              id: raw.id,
-              location: raw.location, 
-              status: raw.status
-            })
-        }
+        && typeof raw.location.longitude != 'undefined'
+        && typeof raw.location.latitude != 'undefined') {
+        place = {longitude: raw.location.longitude, latitude: raw.location.latitude}
+      }
+
+      services.push({name: id, location: place})
     })
+    console.log("loaded " + services.length + " service items")
     then(services)
   })
 }
 
 //Add a node with name at position latLon to svg using the projection
-function addLocation(name, lonLat, svg, projection) {		
+function addMapLocation(name, lonLat, svg, projection) {		
   //var color = svg.getRandomColor();					
-  var node = svg.append("circle")
-    .attr("r",5)
-    .attr('fill',"#ee2222")
-    .attr('color',"#ee2222")
-    .attr('class',name + " eodnNode")
-    .attr('name',name)
-    .attr('location', lonLat)
-    .attr("transform", function() {return "translate(" + projection(lonLat) + ")";});
+  var node = svg.append("g")
+      .attr("transform", function() {return "translate(" + projection(lonLat) + ")";})
+
+      node.append("circle")
+        .attr("r",5)
+        .attr('fill',"#ee2222")
+        .attr('color',"#ee2222")
+        .attr('class',name + " eodnNode")
+        .attr('name',name)
+        .attr('location', lonLat)
+
   //nodeLocationMap[name] = node ;
-  return  node ;
+  return node ;
 }				
+
+
+OFF_MAP = [-72, 40]
+function addOffMapLocation(name, svg, projection) {
+    pair = OFF_MAP 
+    OFF_MAP = [OFF_MAP[0], OFF_MAP[1]+10]
+    node = addMapLocation(name, pair, svg, projection)
+    node.append("text")
+       .attr("dx", function(d){return 10})
+       .attr("dy", function(d){return 4})
+       .text(function(d) {return "t" + name})
+    return node
+}
 
 function mapPoints(svg, projection, elementId) {
   return function(points) {
@@ -59,8 +79,12 @@ function mapPoints(svg, projection, elementId) {
       .attr("id", elementId)
 
     points.forEach(function(item) {
-      pair = [item.location.longitude, item.location.latitude]
-      addLocation(item.name, pair, svg_points, projection)
+      if (item.location.length == 0) {
+        addOffMapLocation(item.name, svg_points, projection)
+      } else{
+        pair = [item.location.longitude, item.location.latitude]
+        addMapLocation(item.name, pair, svg_points, projection)
+      }
     })
   }
 }
@@ -85,7 +109,7 @@ function baseMap(selector, width, height) {
   svg.append("g").attr("id", "states")
   svg.append("g").attr("id", "overlay")
 
-  d3.json("./maps/us.json", function(error, us) {
+  d3.json("../maps/us.json", function(error, us) {
 
     path = d3.geo.path().projection(projection);
 
