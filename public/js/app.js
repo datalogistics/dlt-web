c7ab70923fc2221467104f5bdc839feaad4266c9
Ezra Kissel <ezkissel@indiana.edu>
2015-02-05 23:13:54 -0500

    Cleaning up the loading of the status page.

--- public/js/app.js
+++ public/js/app.js
@@ -9,133 +9,81 @@ angular.module('measurementApp', ['ngRoute', 'angular-loading-bar', 'ngAnimate',
   'appRoutes', 'SliceCtrl', 'SliceService','SocketService', 'EodnCtrl','jsTree.directive','FilesCtrl',
   'DepotCtrl', 'DepotService']
   ).run(function($rootScope, $http, $q, $timeout, $location, Socket, $route) {
-	  var smPromises = [] ;
-	  $rootScope.getServices = function(cb){
-		  smPromises.push(cb);		  
-          };
-      
-    $http.get('/api/services', {cache: true}).success(function(data) {
-      //console.log('HTTP Service Request: ' , data);
-      //console.log(data.length);
 
-      var uniqueServices = [];
-      var services = [];
-
-      for(var i = 0; i < data.length; i++) {
-        if(uniqueServices.indexOf(data[i].id) == -1) {
-          uniqueServices.push(data[i].id);
-	  services.push(data[i]);
-        }
-      }
-
-      //console.log(uniqueServices.length);
-      //console.log(uniqueServices);
-
-      getServices = function() {
-        var promises = [];
-
-	// let's not query each service
-	return $q.all(promises);
-
-        for(var i = 0; i < uniqueServices.length; i++) {
-	  console.log(uniqueServices[i]);
-          promises.push($http.get(uniqueServices[i]).success(function(data) {
-	  //console.log('HTTP Service Request: ', data);
-          services.push(data);
-          }));
-        }
-
-        return $q.all(promises);
+      getUniqueById = function(ary) {
+	  var curr = [];
+	  var ret = [];
+	  for(var i = 0; i < ary.length; i++) {
+              if(curr.indexOf(ary[i].id) == -1) {
+		  curr.push(ary[i].id);
+		  ret.push(ary[i]);
+              }
+	  }
+	  return ret;
       };
-
-      getServices().then(function(data) {
-        console.log('Loading complete, redirecting');
-        for(var i = 0 ; i < smPromises.length ; i++){
-        	try{
-        		smPromises[i](services);
-        	}catch(e){}
-        	
-        };        
-        if(!$rootScope.gotoSomeotherPage) {
-        	$location.path('/status');
-        	$rootScope.services = services;
-        	console.log('root scoping serviuce');
-        	$rootScope.gotoSomeotherPage = false ;
-        }
-
-        // set timer value
-        onTimeout = function() {
-          for(var i = 0; i < services.length; i++) {
-
-            if(services[i].ttl == 0) {
-              services[i].status = 'Unknown';
-            } else if(services[i].ttl < 0) {
-              services[i].status = 'OFF';
-            } else {
-              services[i].ttl--;
-            }
-          }
-          //continue timer
-          timeout = $timeout(onTimeout, 1000);
-        }
-        
-        $rootScope.services = services;
-        // set ttl value
-        for(var i = 0; i < services.length; i++) {
-          var now = Math.round(new Date().getTime() / 1e3) //seconds
-            //console.log("Current time " + now);
-          services[i].ttl = Math.round(((services[i].ttl + (services[i].ts / 1e6)) - now));
-        }
-
-        // start timer
-        var timeout = $timeout(onTimeout, 1000);
-        if(!$rootScope.gotoSomeotherPage) {
-        	// I dont why this is needed
-        	$route.reload();
-        	$location.path('/status');
-        };
-        // open sockets
-        Socket.emit('service_request', {});
-      });
-    });
-
-    $http.get('/api/ports', { cache: true}).success(function(data) {
-      //console.log('Port Request: ' + data);
-
-      Socket.emit('port_request', {});
-
-      $rootScope.ports = data;
-    }).error(function(data) {
-      console.log('Port Error: ' + data);
-    });
-
-    $http.get('/api/nodes', { cache: true}).success(function(data) {
-      //console.log('Node Request: ' + data);
-
-      Socket.emit('node_request', {});
-
-      $rootScope.nodes = data;
-    }).error(function(data) {
-      console.log('Node Error: ' + data);
-    });
-
-    $http.get('/api/measurements' ,{ cache: true}).success(function(data) {
-      //console.log('Measurement Request: ' + data);
-
-      Socket.emit('measurement_request', {});
       
-      $rootScope.measurements = data;
-    }).error(function(data) {
-      console.log('Measurement Error: ' + data);
-    });
+      finish = function() {
+	  var services = $rootScope.services;
 
-    $http.get('/api/metadata', { cache: true}).success(function(data) {
-      //console.log('Metadata Request: ' + data);
+	  console.log('Loading complete, redirecting');
+          if(!$rootScope.gotoSomeotherPage) {
+              $location.path('/status');
+              $rootScope.services = services;
+              console.log('root scoping service');
+              $rootScope.gotoSomeotherPage = false ;
+          }
+	  
+          // set timer value
+          onTimeout = function() {
+              for(var i = 0; i < services.length; i++) {
+		  if(services[i].ttl == 0) {
+		      services[i].status = 'Unknown';
+		  } else if(services[i].ttl < 0) {
+		      services[i].status = 'OFF';
+		  } else {
+		      services[i].ttl--;
+		  }
+              }
+              //continue timer
+              timeout = $timeout(onTimeout, 1000);
+          }
+          
+          $rootScope.services = services;
+          // set ttl value
+          for(var i = 0; i < services.length; i++) {
+              var now = Math.round(new Date().getTime() / 1e3) //seconds
+              //console.log("Current time " + now);
+              services[i].ttl = Math.round(((services[i].ttl + (services[i].ts / 1e6)) - now));
+          }
+	  
+          // start timer
+          var timeout = $timeout(onTimeout, 1000);
+          if(!$rootScope.gotoSomeotherPage) {
+              // I dont why this is needed
+              $route.reload();
+              $location.path('/status');
+          };
+	  Socket.emit('service_request', {});
+      };
+      
+      $q.all([
+	  $http.get('/api/nodes', { cache: true}),
+	  $http.get('/api/ports', { cache: true}),
+	  $http.get('/api/measurements', { cache: true}),
+	  $http.get('/api/metadata', { cache: true}),
+	  $http.get('/api/services', { cache: true})
+      ]).then(function(res) {
+	  $rootScope.nodes = res[0].data;
+	  $rootScope.ports = res[1].data;
+	  $rootScope.measurements = res[2].data;
+	  $rootScope.metadata = res[3].data;
+	  $rootScope.services = getUniqueById(res[4].data);
+	  
+	  Socket.emit('node_request', {});
+	  Socket.emit('port_request', {});
+	  Socket.emit('measurement_request', {});
+	  Socket.emit('metadata_request', {});
 
-      Socket.emit('metadata_request', {});
-
-      $rootScope.metadata = data;
-    }).error(function(data) {
-      console.log('Metadata Error: ' + data);
-    });
+	  finish();
+      });
   });
