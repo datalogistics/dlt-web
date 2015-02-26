78ef2285fb6227a84aa424501c2f2cd275c8c174
Joseph Cottam <jcottam@indiana.edu>
2015-02-26 13:04:35 -0500

    Changed to have a wider variety of source locations and to use new multi-file update names.

--- app/socketTester.js
+++ app/socketTester.js
@@ -10,38 +10,38 @@ var msg = {
 		p : 'eodnDownload_pushData'
 };
 sock.on('connect' , function(){
-    console.log('connected successfully ');
-    sock.emit(msg.r , {
-	hashId : 1244,
-	filename : "test_image.tiff",
-	totalSize : 16777216,
-	connections : 4
-    });
-    var off = 0;
-    setInterval(function(){
-	sock.emit(msg.p, { 
+  console.log('connected successfully ');
+  sock.emit(msg.r , {
+    hashId : 1244,
+    filename : "test_image.tiff",
+    totalSize : 16777216,
+    connections : 4
+  });
+  var off = 0;
+  setInterval(function(){
+    sock.emit(msg.p, { 
 	    hashId : 1244,
 	    ip : 'dresci.incntre.iu.edu',
 	    offset : off,
-	    progress : 65536
+	    amountRead : 65536
 	});
 	sock.emit(msg.p, {
 	    hashId : 1244,
-	    ip : '152.54.14.13',
+	    ip : 'pcvm2-2.utahddc.geniracks.net',
 	    offset : off + 65536,
-	    progress : 32768
-	});
-	sock.emit(msg.p, {
-	    hashId : 1244,
-	    ip : '152.54.14.15',
-	    offset : off + 98304,
-	    progress : 49152
+	    amountRead : 32768
 	});
 	sock.emit(msg.p, {
 	    hashId : 1244,
 	    ip : '155.99.144.103',
 	    offset : off + 98304,
-	    progress : 262144
+	    amountRead : 49152
+	});
+	sock.emit(msg.p, {
+	    hashId : 1244,
+	    ip : '152.54.14.7',
+	    offset : off + 98304,
+	    amountRead : 262144
 	});
 	off = off + 360448;
     },1000);
