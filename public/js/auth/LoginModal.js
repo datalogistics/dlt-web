324270734f3158a94be294c76d0a46292537440f
Prakash <prakraja@umail.iu.edu>
2015-09-07 10:29:47 -0500

    Adding Login modal to UI

--- public/js/auth/LoginModal.js
+++ public/js/auth/LoginModal.js
@@ -1,3 +1,29 @@
-function loginModal($scope) {
+function loginModal($scope,$modalInstance) {
+  function done() {
+    $modalInstance.close();
+  }
+
+  $scope.isLogin = true;
+  $scope.showLogin = function() {  $scope.isLogin = true; };
+  $scope.showRegister = function() { $scope.isLogin = false; };
   
+  $scope.login = function() {
+    alert('das');
+    var isValid = false;
+    if (isValid) 
+      done();
+  }
+
+  $scope.register = function() {
+    alert('reg');
+    var isValid = false;
+    if (isValid) 
+      done();
+  }
+  
+  $scope.closeDialog = function() {
+    $modalInstance.dismiss('cancel');
+  }
 }
+
+
