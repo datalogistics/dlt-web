cab334ff4fd8da4d3a3aeac779625866d6e516ee
Joseph Cottam <jcottam@indiana.edu>
2015-12-10 15:17:20 -0500

    Reasonbly happy with grid view

--- public/js/map/ExnodeMapController.js
+++ public/js/map/ExnodeMapController.js
@@ -27,6 +27,7 @@ function exnodeMapController($scope, $location, $http, UnisService, SocketServic
     if (exnode && exnode.extents) {
       var extents = exnode.extents.map(function(e) {return {id: e.id, offset: e.offset, size: e.size, depot: parseLocation(e.mapping.read)}})
                                   .map(function(e) {e["xy"] = mapLocation(map, e.depot); return e})
+                                  .sort((a,b) => a.depot.localeCompare(b.depot)) 
 
       var fill = d3.scale.category10()
       spokeExtents(map, exnode.size, extents, fill)
@@ -59,7 +60,15 @@ function exnodeMapController($scope, $location, $http, UnisService, SocketServic
 
   function gridmap(map, rootSize, extents, fill) {
     var width = 900
-    var height = 200
+    var height = 250 
+
+    var grid_width = 100 //Number of cells horizontally
+    var grid_height= 5   //Number of rows
+
+    var cell_width_pad = .5    //Distance between adjacent cells
+    var cell_height_pad = 10  //Distance between rows
+    var overlay_height = 5    //Height of duplicate indicator bands
+    
 
     var root = map.svg.append("g").attr("id", "gridmap").attr("transform","translate(100,550)")
 
@@ -70,13 +79,12 @@ function exnodeMapController($scope, $location, $http, UnisService, SocketServic
         .attr("fill", "#FFF")
     
         
-    var grid_width = 100
-    var grid_height= 10 
     var cells = range(0, grid_width*grid_height).map(function(e) {return {depots:new Set(), exnodes:[]}})
 
     extents.forEach(function(e) {
-      var lowCell = Math.floor((e.offset/rootSize)*cells.length)
-      var highCell = Math.min(Math.ceil(((e.offset+e.size)/rootSize)*cells.length), cells.length-1)
+      //var lowCell = Math.min(Math.floor((e.offset/rootSize)*cells.length), cells.length)
+      var lowCell = Math.min(Math.ceil((e.offset/rootSize)*cells.length), cells.length)
+      var highCell = Math.min(Math.ceil(((e.offset+e.size)/rootSize)*cells.length), cells.length)
       range(lowCell, highCell).forEach(function(cell) {
         cells[cell].exnodes.push(e.id)
         cells[cell].depots.add(e.depot)
@@ -85,18 +93,37 @@ function exnodeMapController($scope, $location, $http, UnisService, SocketServic
 
     cells.forEach(e => {e.depots = Array.from(e.depots); e.depots.sort()}) //Ensure unique depot ids and cannonical order
 
-    var sections = root.selectAll(".section").data(cells)
-      
+    var flattened = cells.reduce(function(acc, cell, cell_idx) {
+      var flat = cell.depots.map((d,i) => {return {depot: d, cell_idx: cell_idx, order: i}})
+      acc = acc.concat(flat)
+      return acc
+    }, [])
+
+
+    var sections = root.selectAll(".section").data(flattened.filter(d => d.order == 0))
       .enter().append("rect")
         .attr("class", "section")
-        .attr("exnodes", d => d.exnodes)
-        .attr("depots", d => d.depots)
-        .attr("x", (d,i) => (i%grid_width)*(width/grid_width))
-        .attr("y", (d,i) => Math.floor(i/grid_width)*(height/grid_height))
-        .attr("width", (width/grid_width)-1)
-        .attr("height", (height/grid_height)-1)
-        .attr("fill", (d,i) => fill(d.depots[0]))
+        .attr("x", d => (d.cell_idx%grid_width)*(width/grid_width))
+        .attr("y", d => Math.floor(d.cell_idx/grid_width)*(height/grid_height))
+        .attr("width", (width/grid_width)-cell_width_pad)
+        .attr("height", (height/grid_height)-cell_height_pad)
+        .attr("fill", (d,i) => fill(d.depot))
+    
+        
+    var stack = function(d) {
+      if (d.order == 0) {return (height/grid_height)-2}
+      if (d.order > 5) {return 0}
+      return ((height/grid_height)/7)*d.order
+    }
 
+    var sections = root.selectAll(".subsection").data(flattened.filter(d => d.order < 5 && d.order > 0))
+      .enter().append("rect")
+        .attr("class", "section")
+        .attr("x", d => (d.cell_idx%grid_width)*(width/grid_width))
+        .attr("y", d => Math.floor(d.cell_idx/grid_width)*(height/grid_height)+(d.order-1)*overlay_height)
+        .attr("width", (width/grid_width)-cell_width_pad)
+        .attr("height", overlay_height)
+        .attr("fill", (d,i) => fill(d.depot))
   }
 
   function parseLocation(mapping) {return mapping.split("/")[2]}
