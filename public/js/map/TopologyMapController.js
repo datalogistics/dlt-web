e0280036aa633216f531fb91741f54a04e8bf248
Joseph Cottam <jcottam@indiana.edu>
2016-01-21 19:20:31 -0500

    First tree layout working

--- public/js/map/TopologyMapController.js
+++ public/js/map/TopologyMapController.js
@@ -272,9 +272,43 @@ function treeDraw(map, graph) {
   var svg = map.group
   var width = map.width
   var height = map.height
+  var diameter = width/2
+  var pad = 50
+
+  svg.attr("transform", "translate(" + diameter/2 + ", " + diameter/2 + ")")
 
   var layout = d3.layout.tree()
+                  .children(n => n.children ? n.children : [])
+                  .size([360, diameter/2-pad])
+                  .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
+  var diagonal = d3.svg.diagonal.radial()
+    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+  
+  var nodes = layout.nodes(graph.tree),
+      links = layout.links(nodes);
+
+  var link = svg.selectAll(".link")
+      .data(links)
+      .enter().append("path")
+      .attr("class", "link")
+      .style("stroke-width", "1") //TODO: REDO Sytling/class structure so this is an attr
+      .attr("d", diagonal);
+
+  var node = svg.selectAll(".node")
+      .data(nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+  node.append("circle")
+      .attr("r", 4.5);
+
+  node.append("text")
+      .attr("dy", ".31em")
+      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+      .text(function(d) { return d.name; });
 }
 
 
