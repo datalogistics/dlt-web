308822115560888bc2263c918f72141e2e1b34c4
Joseph Cottam <jcottam@indiana.edu>
2016-03-02 14:29:58 -0500

    Unsatisfying attempt at using the d3 tree layout.

--- public/js/map/TopologyMapController.js
+++ public/js/map/TopologyMapController.js
@@ -199,35 +199,29 @@ function shallowClone(obj) {
 }
 
 // ------------------------- Spoke-based drill down -------------------------
+// This can be used for map overlay...I hope
 function spokeDraw(graph, selection,  svg, width, height, nodeClick) {
   var layout = layoutTree(0, graph.tree, {x: width/2, y: height/2+20}, width/4, {})
   var maxLayer = Object.keys(layout).reduce((acc, n) => Math.max(layout[n].layer, acc), 0)
- 
+
+
+  var tree = d3.layout.tree()
+                .size([360, width/8])
+                .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; })
+
+  var layout = tree.nodes(graph.tree)
+                .map(n => {
+                  var cartesian = toCartesian(n.y, (n.x-90)/180 * Math.PI)
+                  return {x: cartesian.x+width/2, y:cartesian.y+height/2, node: n}})
+                .reduce((acc, entry) => {acc[entry.node.path] = entry; return acc}, {})
+
+
   var nodes = Object.keys(layout).map(k => layout[k].node)
   var colors = domainColors(nodes, svg, 10, 15)
   colors.fn = selectionOverlay(selection, colors.fn)
   nodes = colors.nodes
-
-  //Tree links
-  function getTreeLinks(acc, root) {
-    if (root.children) {
-      root.children.forEach(child => acc.push([root.path, child.path]))
-      root.children.forEach(child => getTreeLinks(acc, child))
-    }
-    return acc
-  }
-  var treeLinks = getTreeLinks([], graph.tree).filter(l => l[0] != "root")
-  var treelink = svg.append("g").attr("id", "tree-links")
-      .selectAll(".tree-link").data(treeLinks)
-      .enter().append("line")
-        .attr("class", "tree-link")
-        .attr("stroke-width", 1) 
-        .attr("stroke", "black")
-        .attr("x1", d => layout[d[0]].x)
-        .attr("y1", d => layout[d[0]].y)
-        .attr("x2", d => layout[d[1]].x)
-        .attr("y2", d => layout[d[1]].y)
-
+  
+  
   var node = svg.selectAll(".tree-node").data(nodes)
   node.enter().append("circle")
     .attr("class", "tree-node")
@@ -254,6 +248,27 @@ function spokeDraw(graph, selection,  svg, width, height, nodeClick) {
      .attr("stroke", "gray")
 
   tooltip(svg, "circle.tree-node")
+
+  //Tree links
+  function getTreeLinks(acc, root) {
+    if (root.children) {
+      root.children.forEach(child => acc.push([root.path, child.path]))
+      root.children.forEach(child => getTreeLinks(acc, child))
+    }
+    return acc
+  }
+  var treeLinks = getTreeLinks([], graph.tree).filter(l => l[0] != "root")
+  var treelink = svg.append("g").attr("id", "tree-links")
+      .selectAll(".tree-link").data(treeLinks)
+      .enter().append("line")
+        .attr("class", "tree-link")
+        .attr("stroke-width", 1) 
+        .attr("stroke", "black")
+        .attr("x1", d => layout[d[0]].x)
+        .attr("y1", d => layout[d[0]].y)
+        .attr("x2", d => layout[d[1]].x)
+        .attr("y2", d => layout[d[1]].y)
+
 
   //SELECTION LINKS
   var selectionPoints = selection.map(s => nodes[pathToIndex(s, nodes)])
