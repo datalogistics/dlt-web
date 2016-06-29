63a7aef5a39ff34fd969567191033db517598ed8
Ezra Kissel <ezkissel@indiana.edu>
2016-06-28 22:46:42 -0400

    Revert "Adding the License headers"
    This reverts commit ae2686091ec5efe59d2e7dcd023b25d0016fcb53.

--- app/cartSocket.js
+++ app/cartSocket.js
@@ -1,15 +1,3 @@
-// =============================================================================
-//  Data Logistics Toolkit (dlt-web)
-//
-//  Copyright (c) 2015-2016, Trustees of Indiana University,
-//  All rights reserved.
-//
-//  This software may be modified and distributed under the terms of the BSD
-//  license.  See the COPYING file for details.
-//
-//  This software was created at the Indiana University Center for Research in
-//  Extreme Scale Technologies (CREST).
-// =============================================================================
 var cfg = require('../properties')
 , q = require('q')
 , xmlparse = require('xml2js').parseString
--- properties.js
+++ properties.js
@@ -1,15 +1,3 @@
-// =============================================================================
-//  Data Logistics Toolkit (dlt-web)
-//
-//  Copyright (c) 2015-2016, Trustees of Indiana University,
-//  All rights reserved.
-//
-//  This software may be modified and distributed under the terms of the BSD
-//  license.  See the COPYING file for details.
-//
-//  This software was created at the Indiana University Center for Research in
-//  Extreme Scale Technologies (CREST).
-// =============================================================================
 /* Configuration file - Configure all source info here .. remove all URL related info from other files */
 var fs = require('fs'),
 _ = require('underscore');
